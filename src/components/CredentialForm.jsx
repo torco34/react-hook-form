9668d7a1a8c9f42214458907f562fd2014d0b7bd
torcoroma torcoromaarias@gmail.com <torcoromaarias@gmail.com>
2024-01-01 19:47:08 -0500

    css for registro losto

--- src/components/CredentialForm.jsx
+++ src/components/CredentialForm.jsx
@@ -22,116 +22,59 @@ export const CredentialForm = ({}) => {
   };
   return (
     <>
-      <br />
       <Row>
-        <Col xs={12} md={6} className="border">
-          hola mundo cruel
-        </Col>
-        <Col xs={12} md={4} className="border">
-          <Form
-            className="credential-form p-5   rounded"
-            onFinish={handleSubmit(onSubmit)}
-          >
-            <div className="input-field">
-              <label>
-                {isRegistration ? "Nombre y apellido" : " Nombre de usuario"}
-              </label>
-              {isRegistration && (
+        <Col xs={12} md={12} className="  containerColumna ">
+          <div className="form-wrapper rounded ">
+            <Form
+              className="credential-form p-5 rounded"
+              onFinish={handleSubmit(onSubmit)}
+            >
+              <div className="input-field">
+                <label>
+                  {isRegistration ? "Nombre y apellido" : " Nombre de usuario"}
+                </label>
+                {isRegistration && (
+                  <Controller
+                    name="username"
+                    control={control}
+                    value=""
+                    rules={{
+                      required: "Este campo es obligatorio",
+                    }}
+                    render={({ field, fieldState }) => (
+                      <div >
+                        <Input
+                          {...field}
+                          placeholder={
+                            isRegistration ? " Nombre y apellidos" : "Usuario "
+                          }
+                          className="input-fields"
+                        />
+                        {fieldState.invalid && (
+                          <p className="error-message">
+                            {fieldState.error?.message}
+                          </p>
+                        )}
+                      </div>
+                    )}
+                  />
+                )}
+                <label>{isRegistration ? "Correo electrónico" : null}</label>
                 <Controller
-                  name="username"
+                  name="usernameOrEmail"
                   control={control}
                   value=""
                   rules={{
                     required: "Este campo es obligatorio",
                   }}
                   render={({ field, fieldState }) => (
-                    <div>
+                    <div className="input-field">
                       <Input
                         {...field}
-                        placeholder={isRegistration ? " Nombre y apellidos" : "Usuario "}
-                      />
-                      {fieldState.invalid && (
-                        <p className="error-message">
-                          {fieldState.error?.message}
-                        </p>
-                      )}
-                    </div>
-                  )}
-                />
-              )}
-              <label>
-               
-                {isRegistration ? "Correo electrónico" : null}
-              </label>
-              <Controller
-                name="usernameOrEmail"
-                control={control}
-                value=""
-                rules={{
-                  required: "Este campo es obligatorio",
-                }}
-                render={({ field, fieldState }) => (
-                  <div>
-                    <Input
-                      {...field}
-                      placeholder={
-                        isRegistration ? " Correo electrónico" : "Usuario "
-                      }
-                    />
-                    {fieldState.invalid && (
-                      <p className="error-message">
-                        {fieldState.error?.message}
-                      </p>
-                    )}
-                  </div>
-                )}
-              />
-            </div>
-
-            <div className="input-field">
-              <label>Contraseña:</label>
-              <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                rules={{
-                  required: "Este campo es obligatorio",
-                  minLength: {
-                    value: 8,
-                    message: "La contraseña debe tener al menos 8 caracteres",
-                  },
-                }}
-                render={({ field, fieldState }) => (
-                  <div>
-                    <Input.Password {...field} placeholder="Contraseña" />
-                    {fieldState.invalid && (
-                      <p className="error-message">
-                        {fieldState.error?.message}
-                      </p>
-                    )}
-                  </div>
-                )}
-              />
-            </div>
-
-            {isRegistration && (
-              <div className="input-field">
-                <label>Confirmar Contraseña:</label>
-                <Controller
-                  name="confirmPassword"
-                  control={control}
-                  defaultValue=""
-                  rules={{
-                    required: "Este campo es obligatorio",
-                    validate: (value) =>
-                      value === getValues("password") ||
-                      "Las contraseñas no coinciden",
-                  }}
-                  render={({ field, fieldState }) => (
-                    <div>
-                      <Input.Password
-                        {...field}
-                        placeholder="Confirmar Contraseña"
+                        placeholder={
+                          isRegistration ? " Correo electrónico" : "Usuario "
+                        }
+                        className="input-fields"
                       />
                       {fieldState.invalid && (
                         <p className="error-message">
@@ -142,16 +85,77 @@ export const CredentialForm = ({}) => {
                   )}
                 />
               </div>
-            )}
-            <Button onClick={handleOnRegistro}>
-              {isRegistration ? "Ingresar" : "Registrarse"}
-            </Button>
-            <div className="submit-button ">
-              <Button type="primary" htmlType="submit">
-                {isRegistration ? "Registrarse" : "Ingresar"}
+
+              <div className="input-field">
+                <label>Contraseña:</label>
+                <Controller
+                  name="password"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "Este campo es obligatorio",
+                    minLength: {
+                      value: 8,
+                      message: "La contraseña debe tener al menos 8 caracteres",
+                    },
+                  }}
+                  render={({ field, fieldState }) => (
+                    <div className="input-field">
+                      <Input.Password
+                        {...field}
+                        placeholder="Contraseña"
+                        className="input-fields"
+                      />
+                      {fieldState.invalid && (
+                        <p className="error-message">
+                          {fieldState.error?.message}
+                        </p>
+                      )}
+                    </div>
+                  )}
+                />
+              </div>
+
+              {isRegistration && (
+                <div className="input-field">
+                  <label>Confirmar Contraseña:</label>
+                  <Controller
+                    name="confirmPassword"
+                    control={control}
+                    defaultValue=""
+                    rules={{
+                      required: "Este campo es obligatorio",
+                      validate: (value) =>
+                        value === getValues("password") ||
+                        "Las contraseñas no coinciden",
+                    }}
+                    render={({ field, fieldState }) => (
+                      <div className="input-field">
+                        <Input.Password
+                          {...field}
+                          placeholder="Confirmar Contraseña"
+                          className="input-fields"
+                        />
+                        {fieldState.invalid && (
+                          <p className="error-message">
+                            {fieldState.error?.message}
+                          </p>
+                        )}
+                      </div>
+                    )}
+                  />
+                </div>
+              )}
+              <Button onClick={handleOnRegistro}>
+                {isRegistration ? "Ingresar" : "Registrarse"}
               </Button>
-            </div>
-          </Form>
+              <div className="submit-button ">
+                <Button type="primary" htmlType="submit">
+                  {isRegistration ? "Registrarse" : "Ingresar"}
+                </Button>
+              </div>
+            </Form>
+          </div>
         </Col>
       </Row>
       <br></br>
